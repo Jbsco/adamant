c31aeabb0c7748546684e1822838d2721c85ae91
Jbsco <90524106+Jbsco@users.noreply.github.com>
2024-03-22 00:27:14 -0600

    plugin command template now applies secondary header definition

--- src/components/ccsds_command_depacketizer/gen/generators/ccsds_commands.py
+++ src/components/ccsds_command_depacketizer/gen/generators/ccsds_commands.py
@@ -36,5 +36,6 @@ class assembly_cosmos_commands_txt(assembly_cosmos_plugin_generator_base, genera
     def generate(self, input_filename):
         a = self.model_cls(input_filename)
         a.ccsds_primary_header_model = model_loader.try_load_model_by_name("Ccsds_Primary_Header")
+        a.ccsds_command_secondary_header = model_loader.try_load_model_by_name("Ccsds_Command_Secondary_Header")
         assembly_gen.create_type_field_strings(a)
         print(a.render(self.template, template_path=self.template_dir))
