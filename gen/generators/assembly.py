a74e2b7c798574721539fcefc86093241f5058d9
Jbsco <90524106+Jbsco@users.noreply.github.com>
2024-05-22 17:51:57 -0600

    COSMOS plugin config file now generated at build/cosmos/template

--- gen/generators/assembly.py
+++ gen/generators/assembly.py
@@ -128,7 +128,7 @@ class assembly_ccsds_cosmos_plugin_txt(assembly_cosmos_plugin_generator_base, ge
         this_file_dir = os.path.dirname(os.path.realpath(__file__))
         template_dir = os.path.join(this_file_dir, ".." + os.sep + "templates")
         assembly_cosmos_plugin_generator_base.__init__(
-            self, "name_ccsds_cosmos_plugin.txt", template_dir=template_dir, subdir="plugin"
+            self, "name_ccsds_cosmos_plugin.txt", template_dir=template_dir, subdir="template"
         )
 
     def generate(self, input_filename):
