8a4990a979fb2c00b38fbf81623630df6b6c52a4
Jacob Seman <90524106+Jbsco@users.noreply.github.com>
2026-03-30 19:08:52 -0600

    Implement baseline python dict generator template for remaining IDed entities

---
+++ gen/templates/assembly/name_commands.py
@@ -0,0 +1,25 @@
+################################################################################
+# {{ formatType(model_name) }} {{ formatType(model_type) }}
+#
+# Generated from {{ filename }} on {{ time }}.
+################################################################################
+{% if commands.items() %}
+
+# Command ID-to-name mapping for the {{ name }} assembly.
+{% for id, command in commands.items() %}
+{{ command.suite.component.instance_name }}_{{ command.name }} = {{ command.id }}
+{% endfor %}
+{% endif %}
+# Reverse lookup: ID to name string
+command_id_to_name = {
+{% for id, command in commands.items() %}
+    {{ command.id }}: "{{ command.suite.component.instance_name }}.{{ command.name }}"{{ "," if not loop.last }}
+{% endfor %}
+}
+
+# Forward lookup: name string to ID
+command_name_to_id = {
+{% for id, command in commands.items() %}
+    "{{ command.suite.component.instance_name }}.{{ command.name }}": {{ command.id }}{{ "," if not loop.last }}
+{% endfor %}
+}
---
+++ gen/templates/assembly/name_parameters.py
@@ -0,0 +1,25 @@
+################################################################################
+# {{ formatType(model_name) }} {{ formatType(model_type) }}
+#
+# Generated from {{ filename }} on {{ time }}.
+################################################################################
+{% if parameters.items() %}
+
+# Parameter ID-to-name mapping for the {{ name }} assembly.
+{% for id, param in parameters.items() %}
+{{ param.suite.component.instance_name }}_{{ param.name }} = {{ param.id }}
+{% endfor %}
+{% endif %}
+# Reverse lookup: ID to name string
+parameter_id_to_name = {
+{% for id, param in parameters.items() %}
+    {{ param.id }}: "{{ param.suite.component.instance_name }}.{{ param.name }}"{{ "," if not loop.last }}
+{% endfor %}
+}
+
+# Forward lookup: name string to ID
+parameter_name_to_id = {
+{% for id, param in parameters.items() %}
+    "{{ param.suite.component.instance_name }}.{{ param.name }}": {{ param.id }}{{ "," if not loop.last }}
+{% endfor %}
+}
